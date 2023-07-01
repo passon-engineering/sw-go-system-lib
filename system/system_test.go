8ae63f0119336a613c412241ba3af504b476e2cc
passont <mail@thomaspasson.de>
2023-07-01 21:37:37 +0200

    Added test

--- system/system_test.go
+++ system/system_test.go
@@ -1,11 +1,13 @@
 package system
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestFunctions(t *testing.T) {
 	testRunCommandGetOutput(t)
+	testRunCommandExist(t)
 }
 
 
@@ -23,4 +25,28 @@ func testRunCommandGetOutput(t *testing.T) {
 
 	// Print the command output for visual inspection
 	t.Logf("Command output: %s", output)
+}
+
+func testRunCommandExist(t *testing.T) {
+	command := "ls"
+	output, err := CommandExists(command)
+	if err != nil {
+		fmt.Printf("Error occurred while checking command existence: %v\n", err)
+	}
+
+	expected := true 
+	if output != expected {
+		t.Errorf("Unexpected command output. Expected: %v, Got: %v", expected, output)
+	}
+
+	command = "fake-command"
+	output, err = CommandExists(command)
+	if err != nil {
+		fmt.Printf("Error occurred while checking command existence: %v\n", err)
+	}
+
+	expected = false
+	if output != expected {
+		t.Errorf("Unexpected command output. Expected: %v, Got: %v", expected, output)
+	}
 }
